75b74a84f83f755f76182b3247a84de82a15d502
ukonpower <ore.ukonpower@gmail.com>
2024-01-08 18:04:12 +0900

    fix

--- src/ts/global.d.ts
+++ src/ts/global.d.ts
@@ -2,18 +2,3 @@ declare module '*.json'{
 	const value: any;
 	export default value;
 }
-
-declare module '*.glsl'{
-	const value: string;
-	export default value;
-}
-declare module '*.vs'{
-	const value: string;
-	export default value;
-}
-declare module '*.fs'{
-	const value: string;
-	export default value;
-}
-
-declare const BASE_PATH: string;
